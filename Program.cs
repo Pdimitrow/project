edb0eebd9c30bad65f8926c6c9e58625c8a81681
Pdimitrow <pdimitrow99@gmail.com>
2024-11-13 00:05:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿int days = int.Parse(Console.ReadLine());
+
+switch (days)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+        case 3:
+        Console.WriteLine("Wednesday");
+        break;
+        case 4:
+        Console.WriteLine("Thursday");
+        break;
+        case 5:
+        Console.WriteLine("Friday");
+        break; 
+        case 6:
+        Console.WriteLine("Saturday");
+        break;
+        case 7:
+        Console.WriteLine("Sunday");
+        break;
+
+}
+
+if  (days >= 5 && days < 5 && days == 5 || days >= 5 && days == 5)
+{
+    Console.WriteLine("Workday");
+}
+else if (days > 5 && days <= 7)
+{
+    Console.WriteLine("Weekend");
+}
+else if (days > 7)
+{
+    Console.WriteLine("Error!");
+    Console.WriteLine($"Please enter a number between 1 and 7.");
+}
